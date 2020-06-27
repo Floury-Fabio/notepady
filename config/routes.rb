5bd83e64e216d514f7b791e324527561d76f3870
floury-fabio <floury-fabio@hotmail.fr>
2020-06-27 17:58:56 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,6 @@ Rails.application.routes.draw do
                  sign_in: 'login',
                  sign_out: 'logout',
                  registration: 'signup'
-               },
-               controllers: {
-                 sessions: 'sessions',
-                 registrations: 'registrations'
                }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
