ee578f430b500f9f8f8b78e3eaf9903294891b3b
floury-fabio <floury-fabio@hotmail.fr>
2020-06-27 12:06:53 +0200

    config user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :notepads
+  has_many :notes, through: :notepads
 end
