d968ff1608af8eaefc5e1afb0a7224fb750b863d
Floury-Fabio <floury-fabio@hotmail.fr>
2020-07-01 09:58:17 +0200

    controllers versionning

---
+++ app/controllers/api/v1/notepads_controller.rb
@@ -0,0 +1,53 @@
+module Api::V1
+  class NotepadsController < ApplicationController
+    before_action :set_notepad, only: [:show, :update, :destroy]
+
+    # GET /notepads
+    def index
+      @notepads = Notepad.all
+
+      render json: @notepads
+    end
+
+    # GET /notepads/1
+    def show
+      render json: @notepad
+    end
+
+    # POST /notepads
+    def create
+      @notepad = Notepad.new(notepad_params)
+
+      if @notepad.save
+        render json: @notepad, status: :created, location: @notepad
+      else
+        render json: @notepad.errors, status: :unprocessable_entity
+      end
+    end
+
+    # PATCH/PUT /notepads/1
+    def update
+      if @notepad.update(notepad_params)
+        render json: @notepad
+      else
+        render json: @notepad.errors, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE /notepads/1
+    def destroy
+      @notepad.destroy
+    end
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_notepad
+      @notepad = Notepad.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def notepad_params
+      params.require(:notepad).permit(:title, :user_id)
+    end
+  end
+end
